f0a56e6e848de6411c6eef88f267dbab54014682
CeliaLet <letruncelia@gmail.com>
2025-06-12 19:02:18 +0200

    Merge effect fixed

--- Assets/Scripts/FruitsManager.cs
+++ Assets/Scripts/FruitsManager.cs
@@ -29,15 +29,52 @@ public class FruitsManager : MonoBehaviour
 				collision.gameObject.SetActive(false);
 				Destroy(collision.gameObject);
 
+				
 				nextFruit = Instantiate(MoveCloud.instance.fruitsPrefabs[fruitIndex+1].prefab);
 				MoveCloud.instance.IncreaseScore(MoveCloud.instance.fruitsPrefabs[fruitIndex + 1].points);
-				
-				nextFruit.transform.position = transform.position;						
 
-				gameObject.SetActive(false);
-				Destroy(gameObject);
+				//Call method to grow and destroy
+				new WaitForSeconds(.75f);
+				
+				nextFruit.transform.position = transform.position;
+
+				StartCoroutine(MergeEffect());
+				//gameObject.SetActive(false);
+				//
+				
+
 			}
+			
 		}
+	}
+
+	IEnumerator MergeEffect()
+	{
+		Collider2D _collider = GetComponent<PolygonCollider2D>();
+		SpriteRenderer _spriteRendr = GetComponent<SpriteRenderer>();
+		Color originalColor = _spriteRendr.color;
+
+		Vector3 originalScale = transform.localScale;
+		Vector3 targetScale = originalScale * 1.2f; // ou une autre valeur de grossissement
+
+		float duration = 0.1f;
+		float elapsed = 0f;
+
+		while (elapsed < duration)
+		{
+			_collider.enabled = false;
+
+			float newAlpha = Mathf.Lerp(1f, 0f, elapsed / duration);
+			_spriteRendr.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+
+			transform.localScale = Vector3.Lerp(originalScale, targetScale, elapsed / duration);
+			elapsed += Time.deltaTime;
+			yield return null;
+		}
+		elapsed = duration;
+		Destroy(gameObject);
+
+
 	}
 
 }
